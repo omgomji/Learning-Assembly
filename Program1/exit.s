f3c50f2f0553b63e9b70e16f69595b29d2eee1d2
omgomji <omjikumar2@gmail.com>
2025-06-11 04:20:00 +0000

    Program 1

---
+++ Program1/exit.s
@@ -0,0 +1,35 @@
+#Purpose: Simple program that exits and returns a
+#         status code back to the Linux Kernel
+#
+
+#Input:   None
+#
+
+#Output:  returns a status code. This can be viewed 
+#         by typing 
+#
+#         echo $?
+#         after running the program
+#
+
+#VARIABLES:
+#         %eax holds the system call number
+#         %ebx hold the return status
+
+.section .data
+
+.section .text
+.global _start
+_start:
+mov $1, %eax     # this is the linux kernel command
+                 # number (system call) for exiting
+                 # a program
+
+mov $13, %ebx     # this is the status number we will 
+                 # return to the operating system.
+                 # Changes this around and it will
+                 # return different things to 
+                 # echo $?
+
+                 # this wakes up the kernel to run
+int $0x80        # the exit command.
